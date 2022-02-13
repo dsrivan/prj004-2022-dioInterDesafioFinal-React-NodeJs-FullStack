379824bc543c1e90ddcdb6fdebe85ef95030b38e
Ivan da Silva Rosa <iivan.si@msn.com>
2022-02-13 18:39:26 -0300

    envio de comentário e exibição no  frontend

--- frontend/src/Pages/product/index.js
+++ frontend/src/Pages/product/index.js
@@ -1,4 +1,5 @@
-import { React } from 'react';
+import axios from "axios";
+import { useState, useEffect } from 'react';
 import { useParams, Link } from "react-router-dom";
 
 import * as S from './styles';
@@ -11,6 +12,14 @@ import SendIcon from '@material-ui/icons/Send';
 const ProductDetails = () => {
     const { id } = useParams();
 
+    const [comments, setComments] = useState([]);
+    const [validator, setValidator] = useState(false);
+    const [message, setMessage] = useState([]);
+    const [author, setAuthor] = useState('');
+    const [content, setContent] = useState('');
+    const [render, setRender] = useState(false);
+    const [success, setSuccess] = useState(false);
+
     const getActualProduct = ({ id_product }) => id_product == id;
     const {
         image,
@@ -18,6 +27,63 @@ const ProductDetails = () => {
         price,
         name_categorys
     } = products.filter(getActualProduct)[0];
+
+    const url = 'http://localhost:5000/message'
+
+    useEffect(() => {
+        axios
+            .get(`${url}/${id}`)
+            .then((response) => {
+                const data = response.data;
+                setComments(data);
+            })
+    }, [render]);
+
+    const sendMessage = () => {
+        setValidator(false);
+
+        if (author.length <= 0 || content.length <= 0) {
+            return setValidator(!validator)
+        }
+        const bodyForm = {
+            email: author,
+            message: content,
+            id_product: id
+        }
+
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(bodyForm)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.id) {
+                    setRender(true);
+                    setSuccess(true);
+                    setTimeout(() => {
+                        setSuccess(false);
+                    }, 5000)
+                }
+            })
+
+        setAuthor('');
+        setContent('');
+    }
+
+    const optionDate = {
+        year: 'numeric',
+        month: 'long',
+        weekday: 'long',
+        // day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        // era: 'long',
+        // timeZoneName: 'long'
+    }
 
     return (
         <S.Container>
@@ -79,40 +145,61 @@ const ProductDetails = () => {
                     <div>
                         <small>Tap your comment below</small>
                         <textarea
+                            id="message"
+                            label="Message"
                             rows={3}
                             maxLength={300}
+                            onChange={(event) => { setContent(event.target.value) }}
                             style={{
                                 width: "100%",
                                 resize: "none",
                                 border: "1px solid lightgray",
                                 borderRadius: "4px",
-                                padding: "4px"
-                            }}
-                        ></textarea>
+                                padding: "10px"
+                            }}>
+                            {content}
+                        </textarea>
                         <small>Tap your best e-mail</small>
                         <input
-                            type="mail"
+                            type="email"
+                            id="name"
+                            label="Name"
                             maxLength={100}
+                            value={author} onChange={(event) => { setAuthor(event.target.value) }}
                             style={{
                                 width: "100%",
                                 resize: "none",
                                 border: "1px solid lightgray",
                                 borderRadius: "4px",
-                                padding: "4px",
+                                padding: "10px",
                                 marginBottom: "20px"
                             }}
                         />
+
+                        {validator &&
+                            <div className="alert alert-warning alert-dismissible fade show mt-2" role="alert">
+                                <strong>Por favor preencha todos os campos!</strong>
+                                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        }
+
+                        {success &&
+                            <div className="alert alert-success alert-dismissible fade show mt-2" role="alert">
+                                <strong>Mensagem foi enviada</strong>
+                            </div>
+                        }
                         <button
+                            onClick={sendMessage}
                             style={{
                                 width: "100%",
                                 border: "1px solid lightgray",
                                 borderRadius: "4px",
-                                padding: "10px 0",
+                                padding: "15px 0",
                                 marginBottom: "20px",
                                 backgroundColor: "#b34c4c",
                                 color: "#fff"
                             }}>
-                            Send your comment
+                            Send comment
                             <SendIcon style={{ marginLeft: "20px" }} />
                         </button>
 
@@ -127,59 +214,40 @@ const ProductDetails = () => {
                         margin: "20px 0"
                     }}>
                         <ul style={{ listStyle: "none", padding: "0" }}>
-                            <li
-                                style={{
-                                    width: "100%",
-                                    // border: "1px solid lightgray",
-                                    backgroundColor: "#f7f7f7",
-                                    borderRadius: "5px",
-                                    padding: "5px",
-                                    marginBottom: "20px"
-                                }}>
-                                <div>
-                                    <p><strong>Fulano@email.com</strong> said:</p>
-                                    <span style={{ marginLeft: "10px" }}>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta tenetur facilis similique quasi aliquid at in doloribus odit ullam quis fugit voluptatibus optio temporibus obcaecati, repudiandae amet!
-                                    </span>
-                                    <p style={{ color: "gray", fontWeight: "bold", display: "block", fontSize: "13px", marginTop: "10px" }}>2022/02/13 13:05</p>
-                                </div>
-                            </li>
-                            <li
-                                style={{
-                                    width: "100%",
-                                    // border: "1px solid lightgray",
-                                    backgroundColor: "#f7f7f7",
-                                    borderRadius: "5px",
-                                    padding: "5px",
-                                    marginBottom: "20px"
-                                }}>
-                                <div>
-                                    <p><strong>Fulano@email.com</strong> said:</p>
-                                    <span style={{ marginLeft: "10px" }}>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta tenetur facilis similique quasi aliquid at in doloribus odit ullam quis fugit voluptatibus optio temporibus obcaecati, repudiandae amet!
-                                    </span>
-                                    <p style={{ color: "gray", fontWeight: "bold", display: "block", fontSize: "13px", marginTop: "10px" }}>2022/02/13 13:05</p>
-                                </div>
-                            </li>
-                            <li
-                                style={{
-                                    width: "100%",
-                                    // border: "1px solid lightgray",
-                                    backgroundColor: "#f7f7f7",
-                                    borderRadius: "5px",
-                                    border: "1px solid lightgray",
-                                    borderRadius: "4px",
-                                    padding: "5px",
-                                    marginBottom: "20px"
-                                }}>
-                                <div>
-                                    <p><strong>Fulano@email.com</strong> said:</p>
-                                    <span style={{ marginLeft: "10px" }}>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta tenetur facilis similique quasi aliquid at in doloribus odit ullam quis fugit voluptatibus optio temporibus obcaecati, repudiandae amet!
-                                    </span>
-                                    <p style={{ color: "gray", fontWeight: "bold", display: "block", fontSize: "13px", marginTop: "10px" }}>2022/02/13 13:05</p>
-                                </div>
-                            </li>
+                            {comments && comments.map(({ id, email, message, created_at }, key) => {
+                                let data = new Date(created_at).toLocaleDateString('pt-br', optionDate)
+                                return (
+                                    <li key={key}
+                                        data-idComment={id}
+                                        style={{
+                                            width: "100%",
+                                            backgroundColor: "#f7f7f7",
+                                            borderRadius: "5px",
+                                            // border: "1px solid lightgray",
+                                            padding: "5px",
+                                            marginBottom: "20px"
+                                        }}>
+                                        <div style={{ padding: "15px" }}>
+                                            <p><strong>{email}</strong> said:</p>
+                                            <span style={{ paddingLeft: "15px" }}>
+                                                {message}
+                                            </span>
+                                            <p style={{
+                                                color: "gray",
+                                                fontWeight: "bold",
+                                                display: "block",
+                                                fontSize: "13px",
+                                                marginTop: "10px"
+                                            }}>
+                                                {/* {
+                                                    `${created_at.split('T')[0]} - ${created_at.split('T')[1]}`
+                                                } */}
+                                                {data}
+                                            </p>
+                                        </div>
+                                    </li>
+                                )
+                            })}
                         </ul>
                     </div>
 
